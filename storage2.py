08235140976a32c62fb5866234d1ac05be038ff8
Ning <ningweizhou1991@gmail.com>
2022-08-05 15:13:23 +0200

    Second commit

---
+++ storage2.py
@@ -0,0 +1,5 @@
+import cbsodata
+import dropbox
+
+dbx = dropbox.Dropbox('sl.BMwUDC47VdoK_GQCUpq6mwVlD18s5W6Enz-ImofkH5UyVAN6JGR6F2l7D-leoMLXw_JAdv0AGw-zbhail8QuoknAdQDoQW1ZkKwIn88527jtZDJlXr5iDyIXzWl0LCSaud3mul-riYGG')
+print(cbsodata.get_table_list())
